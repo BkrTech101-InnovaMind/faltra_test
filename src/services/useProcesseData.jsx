2c28f78813a0ad170fb361b1d9ebad9cd0a9d428
Abobakr Mahdi <zabobaker7355@gmail.com>
2024-07-11 21:56:46 +0300

    modify the use processe data to extract the values in arrays

--- src/services/useProcesseData.jsx
+++ src/services/useProcesseData.jsx
@@ -3,17 +3,29 @@ import useResponseData from "./useResponseData";
 
 export default function useProcessedData() {
   const { data, isLoading, error } = useResponseData();
-  const [processedData, setProcessedData] = useState([]);
+  const [processedData, setProcessedData] = useState({
+    bonus: [],
+    industry: [],
+    location: [],
+  });
 
   useEffect(() => {
     if (!isLoading && !error && data) {
-      const keysAndValues = data.map((item) => {
-        return Object.keys(item).map((key) => ({
-          key,
-          value: item[key],
-        }));
+      const bonusSet = new Set();
+      const industrySet = new Set();
+      const locationSet = new Set();
+
+      data.forEach((item) => {
+        if (item.details.bonus) bonusSet.add(item.details.bonus);
+        if (item.details.industry) industrySet.add(item.details.industry);
+        if (item.details.country) locationSet.add(item.details.country);
       });
-      setProcessedData(keysAndValues);
+
+      setProcessedData({
+        bonus: Array.from(bonusSet),
+        industry: Array.from(industrySet),
+        location: Array.from(locationSet),
+      });
     }
   }, [data, isLoading, error]);
 
