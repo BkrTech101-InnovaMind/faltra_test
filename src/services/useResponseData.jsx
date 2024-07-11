8b3a36e881efb25132064530394036c89a412b1c
Abobakr Mahdi <zabobaker7355@gmail.com>
2024-07-11 06:26:58 +0300

    create a use response hook to fech the json file && return the data with the error and loading state

---
+++ src/services/useResponseData.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+export default function useResponseData() {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch("/api/api");
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  return { data, isLoading, error };
+}
