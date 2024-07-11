39c3cdcb9264405a0962b5605e08d3b2213f2007
Abobakr Mahdi <zabobaker7355@gmail.com>
2024-07-11 08:14:33 +0300

    change the params of the html

--- src/fragments/HomeFragment.jsx
+++ src/fragments/HomeFragment.jsx
@@ -18,7 +18,7 @@ export default function HomeFragment() {
     <WithSideBarLayout>
       <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {data.map((details) => (
-          <DetailsCard key={details.id} details={details} />
+          <DetailsCard key={details.id} details={details.details} />
         ))}
       </section>
       <PaginationBar />
