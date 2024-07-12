7492de3c9c432216178e1151945d7603d248f783
Abobakr Mahdi <zabobaker7355@gmail.com>
2024-07-12 03:22:56 +0300

    change the recived params to be customized from the parent

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,20 +1,18 @@
 import React from "react";
 
-export default function Checkbox({ section, option, optionIdx }) {
+export default function Checkbox({ id, name, checked, onChange, optionText }) {
   return (
     <div className="flex items-center">
       <input
-        id={`${section.id}-${optionIdx}`}
-        name={`${section.id}[]`}
-        defaultValue={option.value}
+        id={id}
+        name={name}
         type="checkbox"
+        checked={checked}
+        onChange={onChange}
         className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
       />
-      <label
-        htmlFor={`${section.id}-${optionIdx}`}
-        className="ml-3 text-sm text-gray-600"
-      >
-        {option.label}
+      <label htmlFor={id} className="ml-3 text-sm text-gray-600">
+        {optionText}
       </label>
     </div>
   );
