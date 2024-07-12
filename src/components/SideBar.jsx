a881f84090d0237cc281fe76424eb683eedcdd95
Abobakr Mahdi <zabobaker7355@gmail.com>
2024-07-12 03:58:10 +0300

    fix the logic error of all filter is makeing a filtering value instead of setting it to nothing

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -12,11 +12,10 @@ function classNames(...classes) {
 
 export default function SideBar({ onFilterChange }) {
   const { processedData } = useProcessedData();
-  const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false);
   const [selectedFilters, setSelectedFilters] = useState({
-    bonus: [],
-    industry: [],
-    location: [],
+    bonus: ["all"],
+    industry: ["all"],
+    location: ["all"],
   });
 
   const handleFilterChange = (filterId, value) => {
@@ -24,16 +23,21 @@ export default function SideBar({ onFilterChange }) {
       let newFilters = { ...prevFilters };
 
       if (value === "all") {
-        newFilters[filterId] = [];
+        newFilters[filterId] = ["all"];
       } else {
         const filterIndex = newFilters[filterId].indexOf(value);
         if (filterIndex === -1) {
+          newFilters[filterId] = newFilters[filterId].filter(
+            (v) => v !== "all"
+          );
           newFilters[filterId].push(value);
         } else {
           newFilters[filterId].splice(filterIndex, 1);
+          if (newFilters[filterId].length === 0) {
+            newFilters[filterId] = ["all"];
+          }
         }
       }
-
       onFilterChange(newFilters);
       return newFilters;
     });
@@ -70,129 +74,8 @@ export default function SideBar({ onFilterChange }) {
   return (
     <div className="bg-white">
       <div>
-        {/* Mobile filter dialog */}
-        <Transition show={mobileFiltersOpen} as={Fragment}>
-          <Dialog
-            as="div"
-            className="relative z-40 md:hidden"
-            onClose={setMobileFiltersOpen}
-          >
-            <Transition.Child
-              as={Fragment}
-              enter="transition-opacity ease-linear duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="transition-opacity ease-linear duration-300"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <div className="fixed inset-0 bg-black bg-opacity-25" />
-            </Transition.Child>
-
-            <div className="fixed inset-0 z-40 flex">
-              <Transition.Child
-                as={Fragment}
-                enter="transition ease-in-out duration-300 transform"
-                enterFrom="translate-x-full"
-                enterTo="translate-x-0"
-                leave="transition ease-in-out duration-300 transform"
-                leaveFrom="translate-x-0"
-                leaveTo="translate-x-full"
-              >
-                <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-6 shadow-xl">
-                  <div className="flex items-center justify-between px-4">
-                    <h2 className="text-lg font-medium text-gray-900">
-                      Filters
-                    </h2>
-                    <button
-                      type="button"
-                      className="-mr-2 flex h-10 w-10 items-center justify-center p-2 text-gray-400 hover:text-gray-500"
-                      onClick={() => setMobileFiltersOpen(false)}
-                    >
-                      <span className="sr-only">Close menu</span>
-                      <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                    </button>
-                  </div>
-
-                  {/* Filters */}
-                  <form className="mt-4">
-                    <SearchFeild />
-                    {filters.map((section) => (
-                      <Disclosure
-                        as="div"
-                        key={section.name}
-                        className="border-t border-gray-200 pb-4 pt-4"
-                      >
-                        {({ open }) => (
-                          <fieldset>
-                            <legend className="w-full px-2">
-                              <Disclosure.Button className="flex w-full items-center justify-between p-2 text-gray-400 hover:text-gray-500">
-                                <span className="text-sm font-medium text-gray-900">
-                                  {section.name}
-                                </span>
-                                <span className="ml-6 flex h-7 items-center">
-                                  <ChevronDownIcon
-                                    className={classNames(
-                                      open ? "-rotate-180" : "rotate-0",
-                                      "h-5 w-5 transform"
-                                    )}
-                                    aria-hidden="true"
-                                  />
-                                </span>
-                              </Disclosure.Button>
-                            </legend>
-                            <Disclosure.Panel className="px-4 pb-2 pt-4">
-                              <div className="space-y-6">
-                                {section.options.map((option) => (
-                                  <div
-                                    key={option.value}
-                                    className="flex items-center"
-                                  >
-                                    <Checkbox
-                                      key={option.value}
-                                      id={`${section.id}-${option.value}-mobile`}
-                                      name={`${section.id}`}
-                                      checked={selectedFilters[
-                                        section.id
-                                      ].includes(option.value)}
-                                      onChange={() =>
-                                        handleFilterChange(
-                                          section.id,
-                                          option.value
-                                        )
-                                      }
-                                      optionText={option.label}
-                                    />
-                                  </div>
-                                ))}
-                              </div>
-                            </Disclosure.Panel>
-                          </fieldset>
-                        )}
-                      </Disclosure>
-                    ))}
-                  </form>
-                </Dialog.Panel>
-              </Transition.Child>
-            </div>
-          </Dialog>
-        </Transition>
-
         <main className="max-w-2xl lg:max-w-7xl">
           <aside>
-            <h2 className="sr-only">Filters</h2>
-            <button
-              type="button"
-              className="inline-flex items-center md:hidden"
-              onClick={() => setMobileFiltersOpen(true)}
-            >
-              <span className="text-sm font-medium text-gray-700">Filters</span>
-              <PlusIcon
-                className="ml-1 h-5 w-5 flex-shrink-0 text-gray-400"
-                aria-hidden="true"
-              />
-            </button>
-
             <div className="hidden md:block">
               <form
                 className="space-y-5 divide-y divide-gray-200"
